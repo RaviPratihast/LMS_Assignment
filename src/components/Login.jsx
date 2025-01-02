e654d22170e8a4003f3b5744e6f7bdf61ca8c81e
Ravi Pratihast <ravipratihast@gmail.com>
2025-01-02 13:06:47 +0530

    test: login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,7 +25,6 @@ const Login = () => {
           Login
         </h2>
         <form onSubmit={handleSubmit} className="space-y-4">
-          {/* Role Selector */}
           <div>
             <label
               htmlFor="role"
@@ -46,7 +45,6 @@ const Login = () => {
             </select>
           </div>
 
-          {/* Email Input */}
           <div>
             <label
               htmlFor="email"
@@ -65,7 +63,6 @@ const Login = () => {
             />
           </div>
 
-          {/* Password Input */}
           <div>
             <label
               htmlFor="password"
@@ -84,7 +81,6 @@ const Login = () => {
             />
           </div>
 
-          {/* Submit Button */}
           <button
             type="submit"
             className="w-full py-2 px-4 bg-blue-500 text-white font-medium rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
